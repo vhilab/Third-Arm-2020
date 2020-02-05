b55cf954766ff9aa339a539db4b591590f0d2027
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-02-05 04:15:46 -0800

    Add button press particle effect

--- gmukobi_thirdarm_v1/Assets/TouchableButton.cs
+++ gmukobi_thirdarm_v1/Assets/TouchableButton.cs
@@ -5,7 +5,8 @@ using UnityEngine.Events;
 
 public class TouchableButton : MonoBehaviour
 {
-    [SerializeField] LayerMask collisionLayerMask = default;
+    [SerializeField] private LayerMask collisionLayerMask = default;
+    [SerializeField] private GameObject particlePrefab = default;
 
     public UnityEvent OnButtonTouched;
 
@@ -15,6 +16,13 @@ public class TouchableButton : MonoBehaviour
         if (((1 << other.gameObject.layer) & collisionLayerMask) != 0)
         {
             //Debug.Log($"Layer matches collision mask!");
+            if (particlePrefab != null)
+            {
+                GameObject particleObject = Instantiate(particlePrefab, transform.position, transform.rotation) as GameObject;
+                particleObject.GetComponent<ParticleSystem>().Play();
+                Destroy(particleObject, 2f);
+            }
+
             OnButtonTouched.Invoke();
         }
     }
