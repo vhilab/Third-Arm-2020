55a3c6de3444fed2d0c2c58fc031a510461fd119
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-02-05 02:42:52 -0800

    Make button game work

---
+++ gmukobi_thirdarm_v1/Assets/TouchableButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TouchableButton : MonoBehaviour
+{
+    [SerializeField] LayerMask collisionLayerMask = default;
+
+    public UnityEvent OnButtonTouched;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & collisionLayerMask) != 0)
+        {
+            OnButtonTouched.Invoke();
+        }
+    }
+}
