2cbc44d99f07930c32b4829551cad07a50b4686f
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-02-12 03:53:21 -0800

    set up (randomized) audio for button game

---
+++ gmukobi_thirdarm_v1/Assets/Scripts/Audio/AudioCuePlayerAtMyPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCuePlayerAtMyPosition : MonoBehaviour
+{
+    [SerializeField] private AudioCueScriptableObject audioCue;
+
+    public void PlayAtMyPosition()
+    {
+        audioCue.Play(transform.position);
+    }
+}
