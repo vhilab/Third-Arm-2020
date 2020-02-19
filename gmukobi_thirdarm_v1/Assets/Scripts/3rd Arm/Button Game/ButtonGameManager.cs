d8d280c9b004f6cfe9d899eb36d5b313d3be2025
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-02-19 03:20:25 -0800

    Simplify button prefab system/third arm game in Unity

--- gmukobi_thirdarm_v1/Assets/Scripts/3rd Arm/Button Game/ButtonGameManager.cs
+++ gmukobi_thirdarm_v1/Assets/Scripts/3rd Arm/Button Game/ButtonGameManager.cs
@@ -5,18 +5,11 @@ using UnityEngine.Events;
 
 public class ButtonGameManager : MonoBehaviour
 {
-    [SerializeField] private TouchableButton button3;
-    [SerializeField] private TouchableButton buttonL;
-    [SerializeField] private TouchableButton buttonR;
+    [SerializeField] private TouchableButton button3 = default;
+    [SerializeField] private TouchableButton buttonL = default;
+    [SerializeField] private TouchableButton buttonR = default;
 
     public UnityEvent OnAllButtonsPushed;
-
-    private MaterialEmissionChanger button3EmissionChanger;
-
-    private void Start()
-    {
-        button3EmissionChanger = button3.gameObject.GetComponentInChildren<MaterialEmissionChanger>();
-    }
 
     private void OnEnable()
     {
@@ -34,16 +27,6 @@ public class ButtonGameManager : MonoBehaviour
 
     void CheckButtonGameState()
     {
-        // make 3rd button light up if both arm buttons are lit up
-        if (buttonL.isBeingTouched && buttonR.isBeingTouched)
-        {
-            button3EmissionChanger.SetEmission(true);
-        }
-        else
-        {
-            button3EmissionChanger.SetEmission(false);
-        }
-
         if (buttonL.isBeingTouched && buttonR.isBeingTouched && button3.isBeingTouched)
         {
             OnAllButtonsPushed.Invoke();
