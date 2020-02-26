a2d5ee5d4bbc01fadac0c2ff16f4025af7ce9d72
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-02-26 13:36:17 -0800

    Kinda get third arm audio and settings working

---
+++ gmukobi_thirdarm_v1/Assets/Scripts/3rd Arm/ThirdArmSettingsReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdArmSettingsReader : MonoBehaviour
+{
+    public ThirdArmSettingsScriptableObject settings;
+    
+    public static ThirdArmSettingsReader Instance;
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+    }
+}
---
+++ gmukobi_thirdarm_v1/Assets/Scripts/Scriptable Objects/ThirdArmSettingsScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+public enum ThirdArmBuildType
+{
+    LabDemo,
+    TribecaFilm
+}
+
+[CreateAssetMenu(fileName = "ThirdArmSettings", menuName = "ScriptableObjects/ThirdArmSettingsScriptableObject", order = 1)]
+public class ThirdArmSettingsScriptableObject : ScriptableObject
+{
+    public ThirdArmBuildType thirdArmBuildType;
+}
