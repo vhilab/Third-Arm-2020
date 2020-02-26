0954fb71c6d99ea298e1e0256f57bc44da823700
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-02-26 14:37:04 -0800

    script for toggling mirror

---
+++ gmukobi_thirdarm_v1/Assets/Scripts/GenericUtility/ToggleActive.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleActive : MonoBehaviour
+{
+    [SerializeField] private bool defaultActive = false;
+
+    private void Start()
+    {
+        gameObject.SetActive(defaultActive);
+    }
+
+    public void ToggleGameObjectActive()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
