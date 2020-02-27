a26e5e52b39d965bea722a2131ff709233aeb1e9
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-02-27 02:58:21 -0800

    Fix bug with locking state toggler while actively togglign

--- gmukobi_thirdarm_v1/Assets/Scripts/Scriptable Objects/ThirdArmSettingsScriptableObject.cs
+++ gmukobi_thirdarm_v1/Assets/Scripts/Scriptable Objects/ThirdArmSettingsScriptableObject.cs
@@ -8,6 +8,6 @@ public enum ThirdArmBuildType
 [CreateAssetMenu(fileName = "ThirdArmSettings", menuName = "ScriptableObjects/ThirdArmSettingsScriptableObject", order = 1)]
 public class ThirdArmSettingsScriptableObject : ScriptableObject
 {
-    public ThirdArmBuildType thirdArmBuildType { get; private set; }
+    public ThirdArmBuildType thirdArmBuildType;
     public float growAngleThresholdHMDEulerX;
 }
