84c658e2f4e601fd7e939d30bd822480568aade0
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-11 02:00:45 -0700

    Add ability to switch between 2 GameObjects as active based on target type (e.g. different IK for film/lab tour)

---
+++ gmukobi_thirdarm_v1/Assets/Scripts/3rd Arm/ToggleBetweenActiveBasedOn3rdArmBuildTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleBetweenActiveBasedOn3rdArmBuildTarget : MonoBehaviour
+{
+    public GameObject[] ObjectsToEnableForLabDemo;
+    public GameObject[] ObjectsToEnableForFilmProject;
+
+    private void Start()
+    {
+        bool isLabDemo = ThirdArmSettingsReader.Instance.settings.thirdArmBuildType == ThirdArmBuildType.LabDemo;
+        foreach (GameObject gameObject in ObjectsToEnableForLabDemo)
+        {
+            gameObject.SetActive(isLabDemo);
+        }
+        foreach (GameObject gameObject in ObjectsToEnableForFilmProject)
+        {
+            gameObject.SetActive(!isLabDemo);
+        }
+    }
+}
